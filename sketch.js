5a90a473c6b3c26febd2a2a3abff7fe648e7e16b
pmueller99 <sadam@impressed.de>
2018-09-28 15:31:21 +0200

    Neu
    Signed-off-by: pmueller99 <sadam@impressed.de>

---
+++ sketch.js
@@ -0,0 +1,83 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
+// Clock
+// Video: https://youtu.be/E4RyStef-gY
+
+const canvasBreite = 700;
+const canvasHoehe = 700;
+
+var myFont;
+function preload() {
+	myFont = loadFont('assets/MythosStd.otf');
+}
+
+function setup() {
+	createCanvas(canvasBreite, canvasHoehe);
+	angleMode(DEGREES);
+}
+
+function draw() {
+	background(0);
+	translate(canvasBreite / 2, canvasHoehe / 2);
+	rotate(-90);
+
+	let hr = hour();
+	let mn = minute();
+	let sc = second();
+
+	strokeWeight(8);
+	stroke(255, 100, 150);
+	noFill();
+	let secondAngle = map(sc, 0, 60, 0, 360);
+	arc(0, 0, 300, 300, 0, secondAngle);
+
+	stroke(150, 100, 255);
+	let minuteAngle = map(mn, 0, 60, 0, 360);
+	arc(0, 0, 280, 280, 0, minuteAngle);
+
+	stroke(150, 255, 100);
+	let hourAngle = map(hr % 12, 0, 12, 0, 360);
+	arc(0, 0, 260, 260, 0, hourAngle);
+
+	push();
+	rotate(secondAngle);
+	stroke(255, 100, 150);
+	line(0, 0, 100, 0);
+	pop();
+
+	push();
+	rotate(minuteAngle);
+	stroke(150, 100, 255);
+	line(0, 0, 75, 0);
+	pop();
+
+	push();
+	rotate(hourAngle);
+	stroke(150, 255, 100);
+	line(0, 0, 50, 0);
+	pop();
+
+	stroke(255);
+	point(0, 0);
+
+	rotate(90);
+
+	noStroke();
+	fill(255);
+	textSize(50);
+	textAlign(CENTER);
+	//textFont(myFont);
+	
+	if (hr < 10) {
+		hr = '0' + hr;
+	}
+	if (mn < 10) {
+		mn = '0' + mn;
+	}
+	if (sc < 10) {
+		sc = '0' + sc;
+	}
+	text(hr + ':' + mn + ':' + sc, 0, canvasHoehe / 2.3);
+}
